b4b87e6ca679f0790629c2a08051d36ce03de02e
MATC <MATC>
2015-10-29 10:25:40 -0600

    investigating displaying code?

--- _js/script.js
+++ _js/script.js
@@ -7,8 +7,10 @@ app.controller('schoolController', ['$scope', function ($scope) {
     $scope.oData = [
         {
             id: 1,
+            file: "_text/001.html",
             course: "javascript-roadtrip-part2",
-            subject: "Loops"
+            subject: "Loops",
+            data: "this is code\nthis is not"
         }, {
             id: 2,
             course: "javascript-roadtrip-part2",
